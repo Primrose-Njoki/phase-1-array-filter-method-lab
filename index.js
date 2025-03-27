84a65e159838e1ac0496bbbbc4d1c5555b5b9c0d
Primrose <primrose.njoki@student.moringaschool.com>
2025-03-27 19:14:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+// Code your solution here
+const users = ["Bobby", "salim", "Mikey", "Peter", "bobby", "Salim"];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+
+console.log(findMatching(users, "bobby"));
+
+function fuzzyMatch(array, string) {
+  let newArray = array.filter((input) => input.startsWith(string[0]));
+  return newArray;
+}
+
+function matchName(array, string) {
+  let newArray = array.filter((input) => input.name === string);
+  return newArray;
+}
